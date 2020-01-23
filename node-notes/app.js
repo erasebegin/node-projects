5cb60b06d21020cf3f4545467141fdb80c314a54
erasebegin <chris.james.haupt@gmail.com>
2020-01-23 20:49:06 +0000

    updated notes app

--- node-notes/app.js
+++ node-notes/app.js
@@ -4,7 +4,7 @@ const yargs = require("yargs");
 
 yargs.command({
   command: "add",
-  describe: "Reading some books",
+  describe: "Add a note",
   builder: {
     title: {
       type: "string",
@@ -17,9 +17,47 @@ yargs.command({
         describe: "Note body"
     }
   },
-  handler: (argv) => {
+  handler (argv) {
     notes.addNote(argv.title,argv.body)
   }
 });
+
+yargs.command({
+  command:"remove",
+  describe:"Remove a note",
+  builder: {
+    title: {
+      type: "string",
+      demandOption: true,
+      describe: "Note title"
+    }
+  },
+  handler (argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler () {
+    notes.listNotes()
+  }
+})
+
+yargs.command({
+  command: "read",
+  describe: "Read the specified note",
+  builder: {
+    title: {
+      type: "string",
+      demandOption: true,
+      describe: "Note title"
+    }
+  },
+  handler (argv) {
+    notes.readNote(argv.title)
+  }
+})
 
 yargs.parse();
--- node-notes/notes.js
+++ node-notes/notes.js
@@ -1,20 +1,38 @@
 const fs = require("fs");
-
-const getNotes = () => {
-  return "My notes...";
-};
+const chalk = require("chalk");
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const filtered = notes.filter(n => n.title === title);
-
-  if (filtered.length === 0) {
+  const hasDuplicate = notes.find(n => n.title === title);
+  if (hasDuplicate.length > 0) {
     notes.push({ title: title, body: body });
     saveNotes(notes);
-    console.log("Note added");
+    console.log(chalk.green.inverse("Note added"));
   } else {
-    console.log("Note title already in use");
+    console.log(chalk.red.inverse("Note title already in use"));
   }
+};
+
+const removeNote = title => {
+  const notes = loadNotes();
+  const filteredNote = notes.find(n => n.title === title);
+  if (filteredNote.length === 0) {
+    console.log(chalk.red.inverse("Could not find a note with that title"));
+  } else {
+    console.log(chalk.green.inverse(`Note "${title}" removed`));
+  }
+  saveNotes(filteredNote);
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  notes.forEach(n => console.log(n.title));
+};
+
+const readNote = title => {
+  const notes = loadNotes();
+  const selectedNote = notes.find(n => n.title === title);
+  console.log(`Title: ${selectedNote.title}\n\n${selectedNote.body}`);
 };
 
 const saveNotes = notes => {
@@ -33,4 +51,9 @@ const loadNotes = () => {
   }
 };
 
-module.exports = { getNotes: getNotes, addNote: addNote };
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote
+};
