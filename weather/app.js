58c8d405fde5a8b2e6429e15e8edabf8cf24ead5
erasebegin <chris.james.haupt@gmail.com>
2020-02-04 08:57:57 +0000

    updating

--- weather/app.js
+++ weather/app.js
@@ -3,23 +3,31 @@ const chalk = require("chalk");
 const geocode = require("./geocode");
 const forecast = require("./forecast");
 
-if(process.argv.length <= 2) {
-    console.log("Please enter a location")
-}
-else {
-    geocode(process.argv[2], (error, geocodeData) => {
-        if (error) {
-          return console.log(error);
-        }
-        
-      
-        forecast(geocodeData.lat, geocodeData.long, (error, forecastData) => {
-          if (error) {
-            return console.log(error);
-          }
-          console.log(chalk.bgYellow.bold(geocodeData.location));
-          console.log(chalk.bgGreen(forecastData.summary));
-          console.log(chalk.bgGreen("Temperature: " + forecastData.temperature + "°C"));
-        });
-      });
+const {argv} = process
+
+if (argv.length <= 2) {
+  console.log("Please enter a location");
+} else {
+  geocode(argv[2], (error, geocodeData) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    const { lat, long, location } = geocodeData;
+
+    forecast(lat, long, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      const { summary, temperature, precipitation } = forecastData;
+
+      console.log(chalk.bgYellow.bold(location));
+      console.log(chalk.bgGreen(summary));
+      console.log(chalk.bgGreen("Temperature: " + temperature + "°C"));
+      console.log(
+        chalk.bgGreen("There is a " + precipitation + "% chance of rain today.")
+      );
+    });
+  });
 }
--- weather/forecast.js
+++ weather/forecast.js
@@ -1,30 +1,36 @@
-const request = require("request")
+const request = require("request");
 
 const forecast = (lat, long, callback) => {
-    const url =
-      "https://api.darksky.net/forecast/4d1bcda0e74126789d66b88bc45b2fd2/" +
-      encodeURIComponent(lat) + "," +
-      encodeURIComponent(long) +
-      "?exclude=minutely,hourly,alerts,flags&units=si";
-  
-    request(
-      {
-        url: url,
-        json: true
-      },
-      (error, response) => {
-        if (error) {
-          callback("Could not connect to weather service", undefined);
-        } else if (response.body.error) {
-          callback("Unable to find location", undefined);
-        } else {
-          callback(undefined, {
-            summary: response.body.daily.summary,
-            temperature: response.body.currently.temperature
-          });
-        }
-      }
-    );
-  };
+  const url =
+    "https://api.darksky.net/forecast/4d1bcda0e74126789d66b88bc45b2fd2/" +
+    encodeURIComponent(lat) +
+    "," +
+    encodeURIComponent(long) +
+    "?exclude=minutely,hourly,alerts,flags&units=si";
 
-  module.exports = forecast
+  request(
+    {
+      url: url,
+      json: true
+    },
+    (error, response) => {
+      const { error: apiError, daily, currently } = response.body;
+      const { temperature, precipProbability: precipitation } = currently;
+      const { summary } = daily;
+
+      if (error) {
+        callback("Could not connect to weather service", undefined);
+      } else if (apiError) {
+        callback("Unable to find location", undefined);
+      } else {
+        callback(undefined, {
+          summary: summary,
+          temperature: temperature,
+          precipitation: precipitation
+        });
+      }
+    }
+  );
+};
+
+module.exports = forecast;
